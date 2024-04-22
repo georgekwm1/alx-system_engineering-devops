de8b6d9700b780d08e334f8e81cf240c457cb568
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-04-22 12:20:38 -0700

    My Commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install flask 2.1.0 using pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# install flask from pip3
+exec { 'install_flask':
+  command => 'pip3 install Flask==2.1',
+  path    => ['/usr/bin'],
+  unless  => 'pip3 show Flask',
 }
