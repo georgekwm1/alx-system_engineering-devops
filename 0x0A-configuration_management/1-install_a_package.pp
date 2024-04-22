a981b8e862309c9aac0ebcb6fdd2028b7175a7eb
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-04-22 12:40:48 -0700

    My Commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,9 @@ exec { 'install_flask':
   path    => ['/usr/bin'],
   unless  => 'pip3 show Flask',
 }
+
+# Install required dependencies
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
