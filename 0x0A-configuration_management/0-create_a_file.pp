eaa1f62796646b71bb175c23fe46e08e1f5065d6
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-04-22 04:43:54 -0700

    My Commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,7 @@
-# creates a file /tmp/school
-file { '/tmp/school':
-  ensure  => present,
+file  { 'school':
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
   mode    => '0744',
-  owner   => www-data,
-  group   => www-data,
   content => 'I love Puppet',
 }
-
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# installs flask 2.1.0 using pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => 'pip3 install Flask==2.1',
+  path    => ['/usr/bin'],
+  unless  => 'pip3 show Flask',
 }
-
