721769be78002a0b3516ea80874e658f43a26ea2
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-06-19 19:13:57 -0700

    My Commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Script that queries subscribers on a given Reddit subreddit.
 """
@@ -7,7 +8,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+
     """Return the total number of subscribers on a given subreddit."""
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
